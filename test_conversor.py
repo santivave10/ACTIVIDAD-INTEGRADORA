4059ca6ed439b16cee521ed32b98cb9aeab15cb5
Santiago Vásquez Vega <112985265+santivave10@users.noreply.github.com>
2026-09-18 18:20:02 -0600

    Todos los archivos finalizados, a falta de TESTEAR

--- test_conversor.py
+++ test_conversor.py
@@ -7,3 +7,29 @@ logging.basicConfig(
     filename = 'test_conversor.log',
     filemode =  'w'
 )
+
+@pytest.mark.system
+@pytest.mark.unit
+def test_celsius_a_fahrenheit():
+    resultado = conversor.celsius_a_fahrenheit(25)
+    assert resultado == 77
+
+@pytest.mark.system
+@pytest.mark.multi
+@pytest.mark.parametrize("quantity, expected", [(10, 6.21371),(50, 31.06855),(1000000000, 621371000)])
+def test_km_a_mi(quantity, expected):
+    resultado = conversor.km_a_mi(quantity)
+    assert resultado == expected
+
+@pytest.mark.system
+@pytest.mark.aprox
+def test_pesomex_a_dolar():
+    resultado = conversor.pesomex_a_dolar(172.30)
+    assert resultado == pytest.approx(10)
+
+@pytest.mark.system
+@pytest.mark.decimal
+def test_celsius_a_fahrenheit_decimal():
+    resultado = conversor.celsius_a_fahrenheit(23.4)
+    assert resultado == 74.12
+
